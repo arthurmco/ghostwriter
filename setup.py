c21b56765ef9d7678c65541adb8466d77a721975
Arthur Mendes <arthurmco@gmail.com>
2017-07-24 23:07:07 -0300

    Minor fix in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="ghostwriter",
     version="0.0.2",
     author="Arthur M",
-    description="a simple article/blog management tool of which *you* show how to show"
+    description="a simple article/blog management tool of which *you* show how to show",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
